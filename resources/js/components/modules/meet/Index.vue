569a25ac9278ea13b161eb5c7bd53bf98fe06c81
Jarvinson <jarvinsonv@gmail.com>
2022-05-09 19:31:10 -0500

    prueba

--- resources/js/components/modules/meet/Index.vue
+++ resources/js/components/modules/meet/Index.vue
@@ -48,9 +48,9 @@
                                         <td v-text="item.date"></td>
                                        
                                         <td>
-                                            <router-link class="btn btn-info btn-sm" :to="{ name: 'meet.edit', params: {id: item.id}}">
+                                            <!-- <router-link class="btn btn-info btn-sm" :to="{ name: 'meet.edit', params: {id: item.id}}">
                                                <i class="fa-solid fa-pen-to-square"></i>
-                                            </router-link >
+                                            </router-link > -->
                                             <router-link class="btn btn-info btn-sm" :name="item.title" :to="{ name: 'meeting', params: {user: user }}">
                                                <i class="fa-solid fa-arrow-right"></i>
                                             </router-link >
